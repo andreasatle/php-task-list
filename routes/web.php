ca02c814c99bb63a051e887290bf6c2f21f67f74
Andreas Atle <atle.andreas@gmail.com>
2023-08-15 15:55:12 -0500

    Remove some unused routes

--- routes/web.php
+++ routes/web.php
@@ -40,22 +40,6 @@ Route::get('/', function () use ($tasks) {
     return view('index', ['tasks' => $tasks]);
 });
 
-// Route to the hello page
-Route::get('/hello', function () {
-    return 'Hello';
-})->name('route-hello');
-
-// Route to the hello page with a name
-Route::get('/greet/{name}', function ($name) {
-    return 'Hello ' . $name . '!';
-});
-
-// Route redirecting to the hello page
-Route::get('/hallo', function () {
-    //return redirect('/hello');
-    return redirect()->route('route-hello');
-});
-
 Route::fallback(function () {
     return 'Page not found';
 });
