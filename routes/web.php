08f90604743ec07a40340c4572146dafcf008e6f
Andreas Atle <atle.andreas@gmail.com>
2023-08-15 13:45:18 -0500

    Finishing first part of routing.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route to the root of the application
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main page';
+});
+
+// Route to the hello page
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('route-hello');
+
+// Route to the hello page with a name
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+// Route redirecting to the hello page
+Route::get('/hallo', function () {
+    //return redirect('/hello');
+    return redirect()->route('route-hello');
+});
+
+Route::fallback(function () {
+    return 'Page not found';
 });
