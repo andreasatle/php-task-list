fff1ee68f11780dee30a68011fc008b8441442ff
Andreas Atle <atle.andreas@gmail.com>
2023-08-16 17:48:39 -0500

    Modify the routes to use the db

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use \App\Models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -14,46 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Temporarily use a class Task, that will be automatically created by some DB magic later.
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed = false,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-// Create some random tasks, will be read from DB later.
-$tasks = [
-    new Task(1, "Kroger", "Shopping", "For Robyn's birthday party", false, "2021-09-01 12:00:00", "2021-09-01 12:00:00"),
-    new Task(2, "Randalls", "Shopping", "Weekly groceries", false, "2021-09-01 12:00:00", "2021-09-01 12:00:00"),
-    new Task(3, "Whole food", "Shopping", "Monthly groceries", false, "2021-09-01 12:00:00", "2021-09-01 12:00:00")
-];
-
 // Route to the home page, redirect to the task list page
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
 // Route to the task list page
-Route::get('/tasks', function () use ($tasks) {
-    return view('index', ['tasks' => $tasks]);
+Route::get('/tasks', function () {
+    // Retrieve all the rows from the DB table, has to be finished
+    // by get() that finalizes the SQL-query.
+    return view('index', ['tasks' => Task::latest()->get()]);
 })->name('tasks.index');
 
 // Route to the task detail page
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    // "Objectify" the task array and match $id
-    $task = collect($tasks)->firstWhere('id', $id);
-    if (!$task) {
-        abort(Response::HTTP_NOT_FOUND, 'Task not found');
-    }
-    return view('show', ['task' => $task]);
+Route::get('/tasks/{id}', function ($id) {
+    // Retrieve the row with $id from the DB table
+    return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::fallback(function () {
